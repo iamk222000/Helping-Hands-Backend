a6778ee987cfac9d60ab92103b9a31442770a124
Kausalya R <68281796+iamk222000@users.noreply.github.com>
2021-09-24 20:58:27 +0530

    Add files via upload

---
+++ src/main/java/com/target/VolunteeringPlatform/PayloadResponse/LoginResponse.java
@@ -0,0 +1,70 @@
+package com.target.VolunteeringPlatform.PayloadResponse;
+
+
+public class LoginResponse {
+    private int id;
+    private String email;
+    private String roles;
+    String firstname;
+    String lastname;
+
+    public LoginResponse(int id, String firstname, String lastname, String email, String roles) {
+        super();
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginResponse{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", roles='" + roles + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
+
