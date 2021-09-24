d4d052f2e0377e5c8069cb0988e3e68c5cde19f4
Kausalya R <68281796+iamk222000@users.noreply.github.com>
2021-09-24 20:55:29 +0530

    Add files via upload

---
+++ src/main/java/com/target/VolunteeringPlatform/DAO/UserRepository.java
@@ -0,0 +1,12 @@
+package com.target.VolunteeringPlatform.DAO;
+
+import com.target.VolunteeringPlatform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+    User findById(int userId);
+    Boolean existsByEmail(String email);
+}
